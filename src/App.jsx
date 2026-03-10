0118aa4c269a92f682be5a8e121cc9a175416fb4
buru33 <rm95327@estudante.fieb.edu.br>
2026-03-10 11:24:58 -0300

    comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//palavra - reservada do jva para criar contantes
+// app - nome do componente, deve começar com letra maiúscula
+// () - sintaxe de função arrow (função anônima) 
 
+function app() {
+   
+  // área do java do componente 
+  // return - palavra reservada do java para retornar o conteúdo do componente
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>{/** Fragmentos estático do componente */}
+      AULA REACTJS DSA2 
+      {/** comentário em JSX */}
     </>
-  )
+  );
 }
+// export(js): exportar componentes
+//default(js): esxportar componentes como padrao
+//app: nome do componente a ser exportado 
+export default app;
 
-export default App
